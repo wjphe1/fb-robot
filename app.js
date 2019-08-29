ab6072ee19d4c2673d2888b5dd612c859bb51fb8
WeiJun <wjphe1@student.monash.edu>
2019-08-29 17:05:14 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1114,11 +1114,11 @@ function sendDateSelection(recipientId) {
 }
 
 function sendLocation(recipientId) {
-  
+
   var address = "Jalan-Raja-Uda"
   var lat = 5.4281314;
   var long = 100.38686;
-  
+
   var messageData = {
     recipient: {
       id: recipientId
@@ -1128,13 +1128,11 @@ function sendLocation(recipientId) {
         "type": "template",
         "payload": {
           "template_type": "generic",
-          "elements": {
-            "element": {
-              "title": "Your current location",
-              "image_url":"https://maps.googleapis.com/maps/api/staticmap?size=764x400&markers=color:blue%7Clabel:S%7C"+address+"&key="+GOOGLEMAPS_API,
-              "item_url": "http://maps.apple.com/maps?q=" + address,
-              "webview_height_ratio": "compact"
-            }
+          "element": {
+            "title": "Your current location",
+            "image_url": "https://maps.googleapis.com/maps/api/staticmap?size=764x400&markers=color:blue%7Clabel:S%7C" + address + "&key=" + GOOGLEMAPS_API,
+            "item_url": "http://maps.apple.com/maps?q=" + address,
+            "webview_height_ratio": "compact"
           }
         }
       }
