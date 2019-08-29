c2c0c5b442760b290b7c9325bfb3ffbf819c833b
WeiJun <wjphe1@student.monash.edu>
2019-08-29 16:59:43 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1116,7 +1116,7 @@ function sendDateSelection(recipientId) {
 function sendLocation(recipientId) {
   
   var address = "Jalan Raja Uda"
-  var address = address.Replace(' ','-')
+  var address = address.split(" ").join("-")
   var lat = 5.4281314;
   var long = 100.38686;
   
